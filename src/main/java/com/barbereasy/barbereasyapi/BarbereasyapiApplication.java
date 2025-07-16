741c6b7033ed3b980ef0e417def49beb694d59d2
FacuFernandez7 <facuufernandez9@gmail.com>
2025-07-16 19:28:35 -0300

    Initial commit

---
+++ src/main/java/com/barbereasy/barbereasyapi/BarbereasyapiApplication.java
@@ -0,0 +1,13 @@
+package com.barbereasy.barbereasyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbereasyapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbereasyapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbereasy/barbereasyapi/BarbereasyapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbereasy.barbereasyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbereasyapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
